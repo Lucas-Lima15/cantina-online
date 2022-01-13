d76540fdf91d095a6ad639759c85a25115a34dd5
Lucas Lima <lucasvinilima15@gmail.com>
2022-01-13 16:07:07 -0300

    Adicionando crsf token nas views

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/login.css')}}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Login</title>
 </head>
 
--- resources/views/responsavel/alunos.blade.php
+++ resources/views/responsavel/alunos.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Alunos</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
